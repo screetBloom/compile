af515a46f35d6f86539e307e8b77ef021d459f80
screetBloom <15306225869@163.com>
2018-09-25 10:30:11 +0800

    loader

--- loader/markdown.js
+++ loader/markdown.js
@@ -5,10 +5,10 @@ let a = 123;
 function f(obj, current, target) {
     let res = JSON.parse(JSON.stringify(obj));
     current.forEach((item, index) => {
-        let reg =new RegExp(`"${item}":`);
-        res.replace(reg, `"${target[index]}":`)
-    })
-
+        let reg =new RegExp(`"${item}":`, 'gm');
+        res = res.replace(reg, `"${target[index]}":`)
+    });
+    return res;
 }
 
 

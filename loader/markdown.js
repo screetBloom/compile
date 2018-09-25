f48b8e3b5a3185d96c085b0e10a23ff1da680b1a
screetBloom <15306225869@163.com>
2018-09-25 10:02:43 +0800

    loader

--- loader/markdown.js
+++ loader/markdown.js
@@ -2,4 +2,14 @@
 let a = 123;
 
 
+function f(obj, current, target) {
+    let res = JSON.parse(JSON.stringify(obj));
+    current.forEach((item, index) => {
+        let reg =new RegExp("^\\d+" + v + "$","gim");
+        res.replace(reg,target[index])
+    })
+
+}
+
+
 

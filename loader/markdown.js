c1077c2673a52bdbb22e70fa24f1b264731b4965
screetBloom <15306225869@163.com>
2018-09-25 13:44:20 +0800

    loader

--- loader/markdown.js
+++ loader/markdown.js
@@ -10,51 +10,17 @@ function f(obj, current, target) {
     return JSON.parse(res);
 }
 
-let test = JSON.stringify({
-    "success": true,
-    "code": 200,
-    "msg": "success",
-    "result": {
-        "productId": 867156,
-        "productName": "韦博的日语考级",
-        "originPrice": {
-            value: 50.00
-        },
-        "currentPrice": {
-            value: 29.00
-        },
-        "hasMoreExpensivePrice": null,
-        "productPic": "//p1.meituan.net/education/7d6c94e872d736a0c52743056397346f357703.jpg%40120w_90h_1e_1c_1l%7Cwatermark%3D0",
-        "expensiveCourse": null,
-        "saleNum": null,
-        "typeName": "日语考级",
-        "classNature": "正式课",
-        "classNumbers": "1对1",
-        "classCat": null,
-        "featureTag": null,
-        "link": "//h5.51ping.com/app/app-education-sku-all/skudetail.html?productId=867156&shopId=b7td7mavw9jg1qg7",
-        "saleTag": "已售5",
-        "hasVideo": null,
-        "videoTag": null,
-        "hasCutPrice": {
-            value: 21.00
-        },
-        "eduDiscountTag": null,
-        "eduDiscountSlogan": null,
-        "eduDiscountPrice": null,
-        "reduceTag": null
-    }
-});
+let test = '';
 
 f1(test, ['originPrice','currentPrice'],[]);
 
 function f1(obj, current, target) {
     let res = JSON.stringify(obj);
     current.forEach((item, index) => {
-        console.log('  >>>>>  ', index);
         let reg = new RegExp(`"${item}":{"value":(.*?)}`, 'gm');
-        console.log(res.match(reg));
-        // res = res.replace(reg, `"${item}":(.*)`);
+        let value = JSON.parse(`{${res.match(reg)[0]}}`)[item].value;
+        res = res.replace(reg, `"${item}":${value}`);
+        console.log(JSON.parse(res))
     });
     return JSON.parse(res);
 }
